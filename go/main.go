ff9af3b41afb2addf3ed8c14ac3210446024c426
adrixop95 <adrixop95@me.com>
2021-08-21 23:06:55 +0200

    go crud example

--- go/main.go
+++ go/main.go
@@ -1,71 +1,113 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 
-	_ "github.com/lib/pq"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/rs/cors"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "test-user"
-	password = "test-password"
-	dbname   = "test-database"
-)
-
-type Profile struct {
+type Driver struct {
+	gorm.Model
 	Name    string
-	Hobbies []string
+	License string
+	Cars    []Car
 }
+
+type Car struct {
+	gorm.Model
+	Year      int
+	Make      string
+	ModelName string
+	DriverID  int
+}
+
+var db *gorm.DB
+var err error
+
+var (
+	drivers = []Driver{
+		{Name: "Jimmy Johnson", License: "ABC123"},
+		{Name: "Howard Hills", License: "XYZ789"},
+		{Name: "Craig Colbin", License: "DEF333"},
+	}
+	cars = []Car{
+		{Year: 2000, Make: "Toyota", ModelName: "Tundra", DriverID: 1},
+		{Year: 2001, Make: "Honda", ModelName: "Accord", DriverID: 1},
+		{Year: 2002, Make: "Nissan", ModelName: "Sentra", DriverID: 2},
+		{Year: 2003, Make: "Ford", ModelName: "F-150", DriverID: 3},
+	}
+)
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	router := mux.NewRouter()
+
+	dsn := "host=localhost port=5432 user=test-user dbname=test-database sslmode=disable password=test-password"
+
+	db, err = gorm.Open("postgres", dsn)
+
 	if err != nil {
-		panic(err)
+		panic("failed to connect database")
 	}
+
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
+	db.AutoMigrate(&Driver{})
+	db.AutoMigrate(&Car{})
+
+	for index := range cars {
+		db.Create(&cars[index])
 	}
 
-	fmt.Println("Successfully connected!")
+	for index := range drivers {
+		db.Create(&drivers[index])
+	}
 
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/foo2", foo2)
-	http.ListenAndServe(":3000", nil)
+	router.HandleFunc("/cars", GetCars).Methods("GET")
+	router.HandleFunc("/cars/{id}", GetCar).Methods("GET")
+	router.HandleFunc("/drivers/{id}", GetDriver).Methods("GET")
+	router.HandleFunc("/cars/{id}", DeleteCar).Methods("DELETE")
+
+	handler := cors.Default().Handler(router)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", handler))
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
-
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+func GetCars(w http.ResponseWriter, r *http.Request) {
+	var cars []Car
+	db.Find(&cars)
+	json.NewEncoder(w).Encode(&cars)
 }
 
-func foo2(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"x", "d"}}
+func GetCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var car Car
+	db.First(&car, params["id"])
+	json.NewEncoder(w).Encode(&car)
+}
 
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func GetDriver(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var driver Driver
+	var cars []Car
+	db.First(&driver, params["id"])
+	db.Model(&driver).Related(&cars)
+	driver.Cars = cars
+	json.NewEncoder(w).Encode(&driver)
+}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+func DeleteCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var car Car
+	db.First(&car, params["id"])
+	db.Delete(&car)
+
+	var cars []Car
+	db.Find(&cars)
+	json.NewEncoder(w).Encode(&cars)
 }
